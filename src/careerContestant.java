c5d488c812960f64700aafdca49ff1a76ae7aefb
tneefjes <tim.neefjes@capgmeini.com>
2018-09-18 12:10:01 +0200

    Constructor of class 'Contestant' adjusted.

--- src/careerContestant.java
+++ src/careerContestant.java
@@ -5,18 +5,8 @@ public class careerContestant extends Contestant{
     private int attackBonus = 10;
 
     public careerContestant(Random myRandom, String gender) {
-        int baseLevel;
-
-        if (gender=="Male") {
-            baseLevel = 100;
-        } else {
-            baseLevel = 90;
-        }
-
-        setGender(gender);
-        setAttackLevel(baseLevel+attackBonus+myRandom.nextGaussian()*10);
-        setDefenseLevel(baseLevel+myRandom.nextGaussian()*10);
-        setHealthLevel(baseLevel+myRandom.nextGaussian()*10);
+        super(myRandom, gender);
+        setAttackLevel(getAttackLevel()+attackBonus);
         setItem(true);
     }
 }
